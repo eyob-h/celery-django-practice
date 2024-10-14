6d278ce859b20c3b138b53664f5068a8a2e947e9
Developer <eyob.hg08@gmail.com>
2024-10-14 17:18:34 +0300

    Add initial celery and django setup

---
+++ mainapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from .tasks import test_task
+from django.http.response import HttpResponse
+
+def test(request):
+    test_task.delay()
+    return HttpResponse("Done")
